b334dc205e46260093f9d14df679246b6b9fb388
Dev4ek <dimadimatrif@gmail.com>
2025-10-26 01:48:33 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://axenix.site",
+        "https://api.idenmarket.com",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
