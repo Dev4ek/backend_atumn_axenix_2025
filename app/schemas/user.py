b49ae7435e890e4c079de8d863cfb3fc11355d65
Dev4ek <dimadimatrif@gmail.com>
2025-10-26 01:04:42 +0300

    fix docker

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class UserResponse(BaseModel):
     id: int = Field(..., description="Уникальный ID пользователя")
     nickname: str = Field(..., description="Имя пользователя")
+    avatar: str = Field(..., description="Ссылка на аватар пользователя")
     created_at: datetime = Field(..., description="Дата и время создания аккаунта")
 
 
